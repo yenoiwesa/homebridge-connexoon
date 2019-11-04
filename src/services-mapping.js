431049b427b92c5a2443db2d18f5af42670cfbc0
Matthieu Di Berardino <matthieu.diberardino@gmail.com>
2019-11-04 22:34:02 +1100

    #5 - Add Roller Shutter support

--- src/services-mapping.js
+++ src/services-mapping.js
@@ -3,4 +3,5 @@ const WindowCovering = require('./services/window-covering');
 module.exports = {
     // maps API device UI Class to a list of services
     Screen: [WindowCovering],
+    RollerShutter: [WindowCovering],
 };
